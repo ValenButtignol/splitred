bca03d87403410925cb06db652e5421b6105361c
ValenButtignol <valentinbuttignol@gmail.com>
2025-07-04 10:24:41 -0300

    add expense db repo

--- infrastructure/db/repository.py
+++ infrastructure/db/repository.py
@@ -2,7 +2,7 @@
 
 from application.ports import UserRepository, GroupRepository, ExpenseRepository
 from domain.models import User, Group, Expense
-from infrastructure.db.models import UserDB, GroupDB
+from infrastructure.db.models import UserDB, GroupDB, ExpenseDB, ExpenseCreditorDB
 
 class SQLAlchemyUserRepository(UserRepository):
     def __init__(self, session):
@@ -57,3 +57,53 @@ class SQLAlchemyGroupRepository(GroupRepository):
         db_group = self.session.query(GroupDB).get(group_id)
         return [User(id=u.id, username=u.username) for u in db_group.members] if db_group else []
 
+class SQLAlchemyExpenseRepository(ExpenseRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, expense: Expense) -> None:
+        db_expense = ExpenseDB(
+            description=expense.description,
+            total_amount=expense.total_amount,
+            group_id=expense.group_id,
+        )
+        self.session.add(db_expense)
+        self.session.flush()  # to obtain expense.id
+
+        # Add debtors (users)
+        for debtor in expense.debtors:
+            db_user = self.session.query(UserDB).get(debtor.id)
+            db_expense.debtors.append(db_user)
+
+        # Add creditors (with amount)
+        for user, amount in expense.creditors:
+            self.session.add(ExpenseCreditorDB(
+                expense_id=db_expense.id,
+                user_id=user.id,
+                amount=amount
+            ))
+
+        self.session.commit()
+        expense.id = db_expense.id # type: ignore
+
+    def list_by_group(self, group_id: int) -> list[Expense]:
+        expenses = self.session.query(ExpenseDB).filter_by(group_id=group_id).all()
+        result = []
+
+        for e in expenses:
+            creditors = [
+                (User(id=c.user_id, username=self.session.get(UserDB, c.user_id).username), c.amount)
+                for c in e.creditors
+            ]
+            debtors = [User(id=u.id, username=u.username) for u in e.debtors]
+
+            result.append(Expense(
+                id=e.id,
+                description=e.description,
+                total_amount=e.total_amount,
+                group_id=e.group_id,
+                creditors=creditors,
+                debtors=debtors
+            ))
+
+        return result
