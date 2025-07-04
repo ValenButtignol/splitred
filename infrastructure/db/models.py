937f8a26cc3ef53e9d3d7ddbaa1bd632b69766f6
ValenButtignol <valentinbuttignol@gmail.com>
2025-07-04 10:24:17 -0300

    add db expense model

--- infrastructure/db/models.py
+++ infrastructure/db/models.py
@@ -13,6 +13,12 @@ group_members = Table(
     Column("user_id", Integer, ForeignKey("users.id"))
 )
 
+expense_debtors = Table(
+    "expense_debtors", Base.metadata,
+    Column("expense_id", Integer, ForeignKey("expenses.id")),
+    Column("user_id", Integer, ForeignKey("users.id")),
+)
+
 class UserDB(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -25,3 +31,22 @@ class GroupDB(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     owner = relationship("UserDB", backref="owned_groups")
     members = relationship("UserDB", secondary=group_members)
+
+class ExpenseDB(Base):
+    __tablename__ = "expenses"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String)
+    total_amount = Column(Float)
+    group_id = Column(Integer, ForeignKey("groups.id"))
+
+    debtors = relationship("UserDB", secondary=expense_debtors)
+
+    creditors = relationship("ExpenseCreditorDB", backref="expense")
+
+# Expense-User intermediate table with amount
+class ExpenseCreditorDB(Base):
+    __tablename__ = "expense_creditors"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    expense_id = Column(Integer, ForeignKey("expenses.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float)
